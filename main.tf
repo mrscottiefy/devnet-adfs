7148b724138860309a5fb2411e8053d08ce9c77c
mrscottiefy <scott.nathaniel@hotmail.com>
2021-06-23 19:36:21 +0800

    added terraform files

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.32.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "dev-aws"
+  region  = "ap-southeast-1"
+}
+
+locals {
+  vpc_id                 = "vpc-02b8bbfcc9e439e23"
+  vpc_cidr_block = "10.0.0.0/16"
+  private_subnet_az_a_id  = "subnet-0b426229916942e24" //sub-a-devnet-sbxezit-IT01
+  private_subnet_az_b_id  = "subnet-0782e33d3e8c212c4" //sub-b-devnet-sbxezit-IT02
+  ec2_key = "scott-keypair"
+}
---
+++ security-group.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "sandbox-managed-ad-terminal-sec-grp" {
+    name = "sgrp-managed-ad-terminal"
+    description = "SG for connecting, joining and administering the managed-ad"
+    vpc_id      = local.vpc_id
+
+  ingress {
+    description = "RDP from OpenVPN Server"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.3.198/32"]
+  }
+   ingress {
+    description = "LDAP from Domain Controllers"
+    from_port   = 389
+    to_port     = 389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SMB from Domain Controllers"
+    from_port   = 445
+    to_port     = 445
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "DNS TCP/UDP from Domain Controllers"
+    from_port   = 53
+    to_port     = 53
+    protocol    = "-1"
+    cidr_blocks = ["${aws_directory_service_directory.sandbox-domain-service.dns_ip_addresses[0]}/32",
+                     "${aws_directory_service_directory.sandbox-domain-service.dns_ip_addresses[1]}/32"
+    ]
+  }
+  egress {
+      description = "All traffic to reosurces within VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [local.vpc_cidr_block]
+  }
+}
