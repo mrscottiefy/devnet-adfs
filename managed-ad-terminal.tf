ac25d9f3946ef8587ac05a725b9177d334f425df
mrscottiefy <scott.nathaniel@hotmail.com>
2021-07-05 12:26:56 +0800

    vpc security group has square brackets []

--- managed-ad-terminal.tf
+++ managed-ad-terminal.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "managed_ad_terminal" {
-  ami             = local.managed_terminal_ami
-  instance_type   = local.managed_terminal_instance_type
-  subnet_id       = local.private_subnet_az_a_mgmt_id
-  key_name        = local.ec2_key
-  security_groups = aws_security_group.sgrp_managed_ad_terminal.id
+  ami                    = local.managed_terminal_ami
+  instance_type          = local.managed_terminal_instance_type
+  subnet_id              = local.private_subnet_az_a_mgmt_id
+  key_name               = local.ec2_key
+  vpc_security_group_ids = [aws_security_group.sgrp_managed_ad_terminal.id]
+
   tags = {
     Name = "vm-devnet-sbxezit-mgmt-terminal-01"
   }
